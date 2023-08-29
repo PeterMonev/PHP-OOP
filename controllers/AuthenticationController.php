2465a9b6e872b3f7848953881756a3099ca375b0
PeterMonev <monevpeter@gmail.com>
2023-08-29 11:32:09 +0300

    Creating login functionality

--- controllers/AuthenticationController.php
+++ controllers/AuthenticationController.php
@@ -3,54 +3,87 @@ require_once '../models/Database.php';
 require_once '../models/User.php';
 require_once '../models/UserManagement.php';
 
-
-
 session_start(); // Starts the session
+
+function isValidEmail($email)
+{
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return false;
+    }
+
+    $pattern = "/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/";
+    return preg_match($pattern, $email);
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     try {
-    $userManager = new UserManagement();
-    
-    // Registration logic
-    if (isset($_POST['register'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $email = $_POST['email'];
-        $role = 'user'; // Default role you 
+        $userManager = new UserManagement();
 
-        // Regisuter Input field validation
-        if(empty($username) || empty($password) || empty($email)) {
-            throw new Exception("All fields are required!");
+        // Registration logic
+        if (isset($_POST['register'])) {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $email = $_POST['email'];
+            $role = 'user'; // Default role you 
+
+            // Regisuter Input field validation
+            if (empty($username) || empty($password) || empty($email)) {
+                throw new Exception("All fields are required!");
+            }
+
+
+            // Validate email format
+            if (!isValidEmail($email)) {
+                throw new Exception("Invalid email format.");
+            }
+
+            if (strlen($password) < 3) {
+                throw new Exception("Password should be at least 3 characters long!");
+            }
+
+            if ($userManager->doesEmailExist($email)) {
+                throw new Exception("Email already registered!");
+            }
+
+            $user = new User(null, $username, $password, $email, $role);
+
+            if (!$userManager->addUser($user)) {
+                throw new Exception("There was an error registering the user.");
+            }
+
+            header("Location: ../views/loginView.php");
+            exit;
         }
-        
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            throw new Exception("Invalid email format!");
+
+        // Login logic
+        if (isset($_POST['login'])) {
+            // Input validation
+            $email = trim($_POST['email']);
+            $password = trim($_POST['password']);
+
+            // Check if email or password fields are empty
+            if (empty($email) || empty($password)) {
+                throw new Exception("Email and password are required.");
+            }
+
+            // Validate email format
+            if (!isValidEmail($email)) {
+                throw new Exception("Invalid email format.");
+            }
+
+            $userData = $userManager->getUserByEmail($email);
+
+            if ($userData && password_verify($password, $userData->getPassword())) {
+                $_SESSION['user_id'] = $userData->getId();
+                header("Location: ../views/profileView.php");
+                exit;
+            } else {
+                throw new Exception("Invalid login.");
+            }
         }
-
-        if (strlen($password) < 3) {
-            throw new Exception("Password should be at least 3 characters long!");
-        }
-
-        if($userManager->doesEmailExist($email)) {
-            throw new Exception("Email already registered!");
-        }
-
-        $user = new User(null, $username, $password, $email, $role);
-
-        if (!$userManager->addUser($user)) {
-            throw new Exception("There was an error registering the user.");
-        }
-        
-        header("Location: ../views/loginView.php");
-        exit;
-    }
-
-
-    } catch (Exception $error){
+    } catch (Exception $error) {
         $_SESSION['error'] = $error->getMessage();
         header("Location: " . $_SERVER['HTTP_REFERER']);
         exit;
-
     }
 }
-
---
+++ views/loginView.php
@@ -0,0 +1,36 @@
+<?php
+include('../includes/header.php');
+session_start();
+?>
+
+<body>
+
+    <div class="container mt-5 w-50 p-5 text-center shadow-lg rounded">
+
+
+        <?php
+        if (isset($_SESSION['error'])) {
+            echo '<div class="alert alert-danger">' . $_SESSION['error'] . '</div>';
+            unset($_SESSION['error']);
+        }
+        ?>
+
+        <h2 class="mb-4">Login</h2>
+        <form action="../controllers/AuthenticationController.php" method="post">
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" name="email" id="email" >
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" class="form-control" name="password" id="password" >
+            </div>
+            <button type="submit" name="login" class="btn btn-info">Login</button>
+            <div class="form-group my-3">
+                <label for="route">You already don't have account? Go to Sign Up.</label>
+                <a href="../views/registerView.php" class="pb-4">Register</a>
+            </div>
+        </form>
+    </div>
+
+</body>
